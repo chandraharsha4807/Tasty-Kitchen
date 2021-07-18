63e188bd55ca6ae126355f7d37be9e2354374e22
chandraharsha <ganivadaharsha998@gmail.com>
2021-07-18 14:08:48 +0000

    initial commit

---
+++ src/components/RestaurantsHeader/index.js
@@ -0,0 +1,41 @@
+import {BsFilterLeft} from 'react-icons/bs'
+import './index.css'
+
+const RestaurantsHeader = props => {
+  const {sortByOptions, activeOptionId, updateActiveOptionId} = props
+  const onChangeSortBy = event => {
+    updateActiveOptionId(event.target.value)
+  }
+  return (
+    <div className="Restaurants-header">
+      <div className="text-container">
+        <h1 className="Restaurants-list-heading">Popular Restaurants</h1>
+        <p className="Restaurants-list-description">
+          Select Your favourite restaurent special dish and make your day
+          happy...
+        </p>
+      </div>
+
+      <div className="sort-by-container">
+        <BsFilterLeft className="sort-by-icon" />
+        <h1 className="sort-by">Sort by</h1>
+        <select
+          className="sort-by-select"
+          value={activeOptionId}
+          onChange={onChangeSortBy}
+        >
+          {sortByOptions.map(eachOption => (
+            <option
+              key={eachOption.optionId}
+              value={eachOption.optionId}
+              className="select-option"
+            >
+              {eachOption.displayText}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+export default RestaurantsHeader
